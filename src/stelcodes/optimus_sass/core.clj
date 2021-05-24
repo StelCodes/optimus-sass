6f68af90ae042839631bcd47eef0439301bce31b
Stel Abrego <stel@stel.codes>
2021-05-23 21:45:13 -0400

    clean up

--- src/stelcodes/optimus_sass/core.clj
+++ src/stelcodes/optimus_sass/core.clj
@@ -3,15 +3,16 @@
             [clojure.string :as str]
             [optimus.assets.creation :refer [last-modified existing-resource]]
             [optimus.assets.load-css :refer [create-css-asset]])
-  (:import [io.bit3.jsass Options])
-  (:gen-class))
+  (:import [io.bit3.jsass Options]))
 
-(defn compile-file [^java.net.URL file output]
+(defn compile-file
+  [^java.net.URL file output]
   (let [compiler (io.bit3.jsass.Compiler.)
         options (Options.)]
     (.getCss (.compileFile compiler (.toURI file) (.toURI output) options))))
 
-(defn load-sass-asset [public-dir path]
+(defn load-sass-asset
+  [public-dir path]
   (let [resource (existing-resource public-dir path)
         new-path (str/replace path #"\.sass\z|\.scss\z" ".css")
         output (.toURL (.toURI (io/file (str public-dir new-path))))]
